9614b3f9c09e5f827afe992ba84d6b7c5d359afb
Tereza Dolejšová <terdoe>
2022-12-12 18:54:57 +0100

    Initial commit

---
+++ src/main/java/cz/czechitas/jw2lekcezoom/Jw2LekceZoomApplication.java
@@ -0,0 +1,13 @@
+package cz.czechitas.jw2lekcezoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jw2LekceZoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jw2LekceZoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/czechitas/jw2lekcezoom/Jw2LekceZoomApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.czechitas.jw2lekcezoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jw2LekceZoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
